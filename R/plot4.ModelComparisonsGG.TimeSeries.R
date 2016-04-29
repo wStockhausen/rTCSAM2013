72bcc64a8876cca299d815b7d6c27b2e183d3c66
wStockhausen <william.stockhausen@noaa.gov>
2016-04-29 09:42:18 -0700

    Revised plot4... function.

--- R/plot4.ModelComparisonsGG.TimeSeries.R
+++ R/plot4.ModelComparisonsGG.TimeSeries.R
@@ -39,7 +39,7 @@ plot4.ModelComparisonsGG.TimeSeries<-function(dfr,
     plots<-list();
     
     #plot with observations & model results
-    if (plotObs&plotMod){
+    if (plotObs&&plotMod){
         p1<-plotModelComparisonsGG.TimeSeries(dfr,
                                               facets=facets,
                                               plotObs=TRUE,
@@ -86,8 +86,8 @@ plot4.ModelComparisonsGG.TimeSeries<-function(dfr,
     }
     
     #plot with observations only
-    if (plotObs){
-        p3<-plotModelComparisonsGG.TimeSeries(dfr,
+    if (plotObs&&(!plotMod)){
+        p1<-plotModelComparisonsGG.TimeSeries(dfr,
                                               facets=facets,
                                               plotObs=TRUE,
                                               plotMod=FALSE,
@@ -99,13 +99,13 @@ plot4.ModelComparisonsGG.TimeSeries<-function(dfr,
                                               xlims=xlims,
                                               ylims=ylims,
                                               showPlot=showPlot);
-        plots$p3<-p3;
+        plots$p1<-p1;
     }
     
     #plot with model results only
-    if (plotMod){
+    if (plotMod&&(!plotObs)){
         #plot full time series
-        p4<-plotModelComparisonsGG.TimeSeries(dfr,
+        p1<-plotModelComparisonsGG.TimeSeries(dfr,
                                               facets=facets,
                                               plotObs=FALSE,
                                               plotMod=TRUE,
@@ -117,7 +117,33 @@ plot4.ModelComparisonsGG.TimeSeries<-function(dfr,
                                               xlims=xlims,
                                               ylims=ylims,
                                               showPlot=showPlot);
-        plots$p4<-p4;
+        plots$p1<-p1;
+        #plot in recent years only
+        xmx<-max(dfr$year,na.rm=TRUE);
+        xplims<-c(xmx-numRecent,xmx+1);
+        if (!is.null(xlims)){
+            xplims[1]<-max(xlims[1],xplims[1],na.rm=TRUE);#max of mins
+            xplims[2]<-min(xlims[2],xplims[2],na.rm=TRUE);#min of maxes
+        }
+        idy<-dfr$year %in% xplims[1]:xplims[2];
+        yplims<-range(dfr$val[idy],na.rm=TRUE,finite=TRUE);
+        if (!is.null(ylims)){
+            yplims[1]<-max(ylims[1],yplims[1],na.rm=TRUE);#max of mins
+            yplims[2]<-min(ylims[2],yplims[2],na.rm=TRUE);#min of maxes
+        }
+        p2<-plotModelComparisonsGG.TimeSeries(dfr,
+                                              facets=facets,
+                                              plotObs=TRUE,
+                                              plotMod=TRUE,
+                                              ci=ci,
+                                              pdfType=pdfType,
+                                              xlab=xlab,
+                                              ylab=ylab,
+                                              title=title,
+                                              xlims=xplims,
+                                              ylims=yplims,
+                                              showPlot=showPlot);
+        plots$p2<-p2;
     }
     return(plots);
 }
